56cc88c806505ce6aac8f30777ac9833ab745120
IJTINK7 <ijtink7@mail.ru>
2023-05-10 21:17:30 +0300

    Changing structure of Todolist

--- src/WishList.tsx
+++ src/WishList.tsx
@@ -14,30 +14,52 @@ export const WishList = (props:WishListPropsType) => {
         <div>
             <h1>{props.wishes.category}</h1>
             <div>
-                <input type="text"/>
-                <input type="text"/>
-                <button>Add</button>
-            </div>
-            <div>
+                <input type="text" placeholder={"Enter an item"}/>
                 <select>
                     <option selected>OS</option>
                     <option value="Android">Android</option>
                     <option value="iOS">iOS</option>
                     <option value="Windows Phone">Windows Phone</option>
                 </select>
+                <input type="text" placeholder={"Enter a price"}/>
+                <button>Add</button>
             </div>
             <ul>
                 {props.wishes.data.map(el=>{
                     return(
                         <li key={el.id}>
                             <input type="checkbox" checked={el.checked}/>
-                            <span> {el.title} </span>
+                            <span onDoubleClick={()=>{alert("Ho")}}> {el.title} </span>
+                            <span> / OS: </span>
+                            <span onDoubleClick={()=>{alert("He")}}> {el.OS} </span>
+                            <span> / price: </span>
+                            <span onDoubleClick={()=>{alert("Hi")}}> {el.price} </span>
                             <button>X</button>
                         </li>
                     )
                 })}
             </ul>
+            <div>
+                FILTER BY:
+                <div>
+                    <select>
+                        <option selected>OS</option>
+                        <option value="Android">Android</option>
+                        <option value="iOS">iOS</option>
+                        <option value="Windows Phone">Windows Phone</option>
+                    </select>
+                </div>
+                <div>
+                    <select>
+                        <option selected>Price</option>
+                        <option value="Android"> ≤ 500</option>
+                        <option value="iOS">500-1000</option>
+                        <option value="Windows Phone">≥1000</option>
+                    </select>
+                </div>
+            </div>
         </div>
+
     );
 
 }
