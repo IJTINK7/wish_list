81c113ac8ce062da77ccd9c5ad2c32628892695b
IJTINK7 <ijtink7@mail.ru>
2023-05-10 21:17:19 +0300

    Changing structure of data

--- src/App.tsx
+++ src/App.tsx
@@ -13,21 +13,25 @@ type PhoneDataPropsType = {
 }
 export type WishesType = {
   category: string
+  OS: string;
+  price: number
   data: PhoneDataPropsType[]
 }
 
 function App() {
   const [wishes, setWishes] = useState<WishesType>({
-    category: "Phone",
-    data: [
-      {id: 1, title: 'IPhone 13 ProMax', OS: "iOS", price: 1200, checked: false},
-      {id: 2, title: 'Iphone 14', OS: "iOS", price: 1400, checked: false},
-      {id: 3, title: 'Samsung Galaxy Fold 4', OS: "Android", price: 1500, checked: false},
-      {id: 4, title: 'Samsung Galaxy S23', OS: "Android", price: 1300, checked: false},
-      {id: 5, title: 'Xiaomi 13', OS: "Android", price: 1000, checked: false},
-      {id: 6, title: 'Huawei', OS: "Android", price: 900, checked: false},
-      {id: 7, title: 'Nokia E7', OS: "Windows Phone", price: 100, checked: false},
-    ]
+      category: "Phones",
+      OS: "123",
+      price: 1233,
+      data: [
+        {id: 1, title: 'IPhone 13 ProMax', OS: "iOS", price: 1200, checked: false},
+        {id: 2, title: 'Iphone 14', OS: "iOS", price: 1400, checked: false},
+        {id: 3, title: 'Samsung Galaxy Fold 4', OS: "Android", price: 1500, checked: false},
+        {id: 4, title: 'Nokia Lumia', OS: "Windows Phone", price: 200, checked: false},
+        {id: 5, title: 'Samsung Galaxy S23', OS: "Android", price: 1300, checked: false},
+        {id: 6, title: 'Xiaomi 13', OS: "Android", price: 1000, checked: false},
+        {id: 7, title: 'Huawei', OS: "Android", price: 900, checked: false},
+      ]
   })
   return (
     <div className="App">
