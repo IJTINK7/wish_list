78b5904f669c3889db4e581186bb48043714094b
IJTINK7 <ijtink7@mail.ru>
2023-05-10 18:24:26 +0300

    Changing the structure of Todolist

--- src/WishList.tsx
+++ src/WishList.tsx
@@ -1,23 +1,41 @@
 import React from 'react';
 import {WishesType} from "./App";
 
+
 export type WishListPropsType = {
 
-    wishes: WishesType[]
+
+    wishes: WishesType
 
 }
 
 export const WishList = (props:WishListPropsType) => {
-
     return (
         <div>
-            <h1>{props.wishes[5].category}</h1>
+            <h1>{props.wishes.category}</h1>
+            <div>
+                <input type="text"/>
+                <input type="text"/>
+                <button>Add</button>
+            </div>
+            <div>
+                <select>
+                    <option selected>OS</option>
+                    <option value="Android">Android</option>
+                    <option value="iOS">iOS</option>
+                    <option value="Windows Phone">Windows Phone</option>
+                </select>
+            </div>
             <ul>
-                <li >
-                    <input type='checkbox' checked={props.wishes[0].checked}/>
-                    <span>{props.wishes[3].title}</span> {` - ${props.wishes[3].OS} - ${props.wishes[3].price}   `}
-                    <button>х</button>
-                </li>
+                {props.wishes.data.map(el=>{
+                    return(
+                        <li key={el.id}>
+                            <input type="checkbox" checked={el.checked}/>
+                            <span> {el.title} </span>
+                            <button>X</button>
+                        </li>
+                    )
+                })}
             </ul>
         </div>
     );
