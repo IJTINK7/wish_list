814971084675e95154c7fc42908590c0d0ffd5b5
IJTINK7 <ijtink7@mail.ru>
2023-05-11 20:48:23 +0300

    Changing structure of Wishlist for lesson

--- src/App.tsx
+++ src/App.tsx
@@ -2,44 +2,37 @@ import React, {useState} from 'react';
 import './App.css';
 import {WishList} from "./WishList";
 
-export type OsType = "iOS" | "Android" | "Windows Phone"
+export type OsType = "All" | "iOS" | "Android"
 
 type PhoneDataPropsType = {
-  id: number
-  title: string
-  OS: OsType
-  price: number
-  checked: boolean
+	id: number
+	title: string
+	OS: OsType
+	price: number
+	checked: boolean
 }
 export type WishesType = {
-  category: string
-  OS: string;
-  price: number
-  data: PhoneDataPropsType[]
+	category: string
+	data: PhoneDataPropsType[]
 }
 
 function App() {
-  const [wishes, setWishes] = useState<WishesType>({
-      category: "Phones",
-      OS: "123",
-      price: 1233,
-      data: [
-        {id: 1, title: 'IPhone 13 ProMax', OS: "iOS", price: 1200, checked: false},
-        {id: 2, title: 'Iphone 14', OS: "iOS", price: 1400, checked: false},
-        {id: 3, title: 'Samsung Galaxy Fold 4', OS: "Android", price: 1500, checked: false},
-        {id: 4, title: 'Nokia Lumia', OS: "Windows Phone", price: 200, checked: false},
-        {id: 5, title: 'Samsung Galaxy S23', OS: "Android", price: 1300, checked: false},
-        {id: 6, title: 'Xiaomi 13', OS: "Android", price: 1000, checked: false},
-        {id: 7, title: 'Huawei', OS: "Android", price: 900, checked: false},
-      ]
-  })
-  return (
-    <div className="App">
-      <WishList wishes={wishes}/>
-    </div>
-  );
+	const [wishes, setWishes] = useState<WishesType>({
+		category: "Phones",
+		data: [
+			{id: 1, title: 'Samsung Galaxy S23', OS: "Android", price: 1300, checked: false},
+			{id: 2, title: 'IPhone 13 ProMax', OS: "iOS", price: 1200, checked: false},
+			{id: 3, title: 'Xiaomi 13', OS: "Android", price: 1000, checked: false},
+			{id: 4, title: 'Huawei', OS: "Android", price: 900, checked: false},
+			{id: 5, title: 'Iphone 14', OS: "iOS", price: 1400, checked: false},
+		]
+	})
+	return (
+		<div className="App">
+			<WishList wishes={wishes}/>
+		</div>
+	);
 }
-
 
 export default App;
 
--- src/WishList.tsx
+++ src/WishList.tsx
@@ -1,65 +1,51 @@
 import React from 'react';
 import {WishesType} from "./App";
 
-
 export type WishListPropsType = {
-
-
-    wishes: WishesType
-
+	wishes: WishesType
 }
 
-export const WishList = (props:WishListPropsType) => {
-    return (
-        <div>
-            <h1>{props.wishes.category}</h1>
-            <div>
-                <input type="text" placeholder={"Enter an item"}/>
-                <select>
-                    <option selected>OS</option>
-                    <option value="Android">Android</option>
-                    <option value="iOS">iOS</option>
-                    <option value="Windows Phone">Windows Phone</option>
-                </select>
-                <input type="text" placeholder={"Enter a price"}/>
-                <button>Add</button>
-            </div>
-            <ul>
-                {props.wishes.data.map(el=>{
-                    return(
-                        <li key={el.id}>
-                            <input type="checkbox" checked={el.checked}/>
-                            <span onDoubleClick={()=>{alert("Ho")}}> {el.title} </span>
-                            <span> / OS: </span>
-                            <span onDoubleClick={()=>{alert("He")}}> {el.OS} </span>
-                            <span> / price: </span>
-                            <span onDoubleClick={()=>{alert("Hi")}}> {el.price} </span>
-                            <button>X</button>
-                        </li>
-                    )
-                })}
-            </ul>
-            <div>
-                FILTER BY:
-                <div>
-                    <select>
-                        <option selected>OS</option>
-                        <option value="Android">Android</option>
-                        <option value="iOS">iOS</option>
-                        <option value="Windows Phone">Windows Phone</option>
-                    </select>
-                </div>
-                <div>
-                    <select>
-                        <option selected>Price</option>
-                        <option value="Android"> ≤ 500</option>
-                        <option value="iOS">500-1000</option>
-                        <option value="Windows Phone">≥1000</option>
-                    </select>
-                </div>
-            </div>
-        </div>
-
-    );
-
+export const WishList = (props: WishListPropsType) => {
+	return (
+		<div>
+			<h1>{props.wishes.category}</h1>
+			<div>
+				<input type="text" placeholder={"Enter an item"}/>
+				<select>
+					<option selected>Select OS</option>
+					<option value="Android">Android</option>
+					<option value="iOS">iOS</option>
+					<option value="Windows Phone">Windows Phone</option>
+				</select>
+				<button>Add</button>
+			</div>
+			<ul>
+				{props.wishes.data.map(el => {
+					return (
+						<li key={el.id}>
+							<input type="checkbox" checked={el.checked}/>
+							<span onDoubleClick={() => {
+								alert("Ho")
+							}}> {el.title} </span>
+							<span> / OS: </span>
+							<span onDoubleClick={() => {
+								alert("He")
+							}}> {el.OS} </span>
+							<button>X</button>
+						</li>
+					)
+				})}
+			</ul>
+			<div>
+				FILTER BY:
+				<div>
+					<select>
+						<option selected>All</option>
+						<option value="Android">Android</option>
+						<option value="iOS">iOS</option>
+					</select>
+				</div>
+			</div>
+		</div>
+	);
 }
