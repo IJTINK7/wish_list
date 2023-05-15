b088cf7f38bab458c93d1be199322938d9b291d8
IJTINK7 <ijtink7@mail.ru>
2023-05-15 09:56:35 +0300

    Preparing for the lesson

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,9 @@
 import React, {useState} from 'react';
 import './App.css';
-import {WishList} from "./WishList";
+import {OsTypeForSelect, WishList} from "./WishList";
 import {v1} from "uuid";
 
-export type OsType = "All" | "iOS" | "Android"
+export type OsType = "All" | "iOS" | "Android" | OsTypeForSelect
 
 export type WishesDataPropsType = {
 	id: string
@@ -11,8 +11,8 @@ export type WishesDataPropsType = {
 	OS: OsType
 	checked: boolean
 }
-
 function App() {
+	const [osFilter, setOsFilter] = useState<OsType>("All")
 	const [wishes, setWishes] = useState<WishesDataPropsType[]>([
 		{id: v1(), title: 'Samsung Galaxy S23', OS: "Android", checked: true},
 		{id: v1(), title: 'IPhone 13 ProMax', OS: "iOS", checked: true},
@@ -21,18 +21,21 @@ function App() {
 		{id: v1(), title: 'Iphone 14', OS: "iOS", checked: false},
 	])
 
-	const addWish = (newTitle: string, newOs: OsType) => {
-		const newWish:WishesDataPropsType = {id: v1(), title: newTitle, OS: newOs, checked: false}
-		setWishes([newWish, ...wishes])
+	const addItem = (newItem: string, wishFilter: OsTypeForSelect) => {
+		let newWishItem = {id: v1(), title: newItem, OS: wishFilter, checked: false};
+		setWishes([newWishItem, ...wishes]);
 	}
 
-	return (
-		<div className="App">
-			<WishList wishes={wishes}
-					  addWish={addWish}
-			/>
-		</div>
-	);
-}
 
+
+	const wishesWhatWeWantToSee = osFilter === "All" ? wishes : wishes.filter(el=> el.OS === osFilter)
+		return (
+			<div className="App">
+				<WishList wishes={wishesWhatWeWantToSee}
+						  addItem={addItem}
+						  osFilter={osFilter}
+				/>
+			</div>
+		);
+	}
 export default App;
--- src/WishList.tsx
+++ src/WishList.tsx
@@ -1,63 +1,79 @@
-import React, {ChangeEvent, useState} from 'react';
+import React, {ChangeEvent, KeyboardEvent, useState} from 'react';
 import {OsType, WishesDataPropsType} from "./App";
+export type OsTypeForSelect = "Android" | "iOS" | "Select OS"
 
 export type WishListPropsType = {
 	wishes: WishesDataPropsType[]
-	addWish: (newTitle: string, newOs: OsType)=> void
+	addItem: (newItem: string, wishFilter: OsTypeForSelect)=> void
+
+	osFilter: OsType
 }
 
 export const WishList = (props: WishListPropsType) => {
-	const [newTitle, setNewTitle] = useState<string>("")
-	const [newOs, setNewOs] = useState<OsType>("All")
-	const addWishHandler = (e: ChangeEvent<HTMLInputElement>) => {
-		setNewTitle(e.currentTarget.value)
+	let [newItem, setNewItem] = useState("")
+	let [os, setOS] = useState<OsTypeForSelect>("Select OS")
+
+	const onNewItemChangeHandler = (e: ChangeEvent<HTMLInputElement>) => {
+		setNewItem(e.currentTarget.value)
 	}
-	const addNewTitleHandler = () => {
-		props.addWish(newTitle.trim(), newOs)
-		setNewTitle("")
+
+
+	const addItemHandler = () => {
+		if(os !== "Select OS"){
+			if (newItem.trim() !== "") {
+				props.addItem(newItem, os)
+				setNewItem("")
+				setOS("Select OS")
+			}
+		}
+		else return
 	}
-	const changeOSHandler = (e: ChangeEvent<HTMLSelectElement>) => {
-		setNewOs(e.currentTarget.value as OsType)
+
+	const onChangeOSHandler = (e:ChangeEvent<HTMLSelectElement>) => {
+		setOS(e.currentTarget.value as OsTypeForSelect)
 	}
+
+
+
+
 	return (
-		<div>
-			<h1>Phones</h1>
 			<div>
-				<input placeholder={"Enter an item"}
-					   value={newTitle}
-					   onChange={addWishHandler}
-				/>
-
-				<select value={newOs} onChange={changeOSHandler}>
-					<option selected value="All">Select OS</option>
-					<option value="Android">Android</option>
-					<option value="iOS">iOS</option>
-				</select>
-
-
-				<button onClick={addNewTitleHandler}>Add</button>
-			</div>
-			<ul>
-				{props.wishes.map(el=>{
-					return <li key={el.id}><input type="checkbox"
-									  checked={el.checked}/>
-						<span> {el.title}</span>
-						<span> / OS: </span>
-						<span> {el.OS} </span>
-						<button>X</button>
-					</li>
-				})}
-			</ul>
-			<div>
-				FILTER BY:
+				<h1>Phones</h1>
 				<div>
-					<select>
-						<option value="All">All</option>
-						<option value="Android">Android</option>
-						<option value="iOS">iOS</option>
+					<input placeholder={"Enter an item"}
+						   value={newItem}
+						   onChange={onNewItemChangeHandler}
+						   />
+					<select value={os} onChange={onChangeOSHandler}>
+						<option value={"Select OS"}>Select OS</option>
+						<option value={"Android"}>Android</option>
+						<option value={"iOS"}>iOS</option>
 					</select>
+					<button onClick={addItemHandler}>Add</button>
+				</div>
+				<ul>
+					{props.wishes.map(el => {
+						return (
+							<li key={el.id}>
+								<input type="checkbox" checked={el.checked}/>
+								<span> {el.title} </span>
+								<span> / OS: </span>
+								<span> {el.OS} </span>
+								<button >X</button>
+							</li>
+						)
+					})}
+				</ul>
+				<div>
+					FILTER BY:
+					<div>
+						<select >
+							<option value="All">All</option>
+							<option value="Android">Android</option>
+							<option value="iOS">iOS</option>
+						</select>
+					</div>
 				</div>
 			</div>
-		</div>
-	);
+		);
 }
