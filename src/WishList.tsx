7a629b8b5f0d30a03e6bfe1d2f30fa3bff6365ba
IJTINK7 <ijtink7@mail.ru>
2023-05-22 19:15:46 +0300

    Finishing "WishList 05" lesson

--- src/WishList.tsx
+++ src/WishList.tsx
@@ -8,18 +8,21 @@ export type WishListPropsType = {
 	wishes: WishesDataPropsType[]
 	osFilter: OsType
 	setOsFilter: (text: OsType) => void
-	statusFilter: StatusTypeForSelect
-	setStatusFilter: (text: StatusTypeForSelect) => void
 	addNewWish: (oS: OsTypeForSelect) => void
 	newWishTitle: string
 	setNewWishTitle: (text: string) => void
 	removeWish: (id: string) => void
+	activityFilter : StatusTypeForSelect
+	setActivityFilter: (filterValue: StatusTypeForSelect) => void
+	changeWishStatus: (wishId: string, statusValue: boolean) => void
 }
 
 export const WishList = (props: WishListPropsType) => {
-	let [oS, setOS] = useState<OsTypeForSelect>("Select OS")
+	const [error, setError] = useState<string | null>(null)
+	const [oS, setOS] = useState<OsTypeForSelect>("Select OS")
 	const onNewItemChangeHandler = (e: ChangeEvent<HTMLInputElement>) => {
 		props.setNewWishTitle(e.currentTarget.value)
+		setError(null)
 	}
 
 	const addWishHandler = () => {
@@ -28,8 +31,9 @@ export const WishList = (props: WishListPropsType) => {
 				props.addNewWish(oS)
 				props.setNewWishTitle("")
 				setOS("Select OS")
-			} else return
-		}
+
+			} else setError("Select item")
+		} else setError("Select OS")
 	}
 	const onKeyDownHandler = (e: KeyboardEvent<HTMLInputElement>) => {
 		if (e.key === "Enter") {
@@ -41,12 +45,19 @@ export const WishList = (props: WishListPropsType) => {
 	}
 	const onChangeOSHandler = (e: ChangeEvent<HTMLSelectElement>) => {
 		setOS(e.currentTarget.value as OsTypeForSelect)
+		setError(null)
 	}
 	const onChangeFilterOSHandler = (e: ChangeEvent<HTMLSelectElement>) => {
 		props.setOsFilter(e.currentTarget.value as OsType)
 	}
-	const onChangeFilterStatusHandler = (e: ChangeEvent<HTMLSelectElement>) => {
-		props.setStatusFilter(e.currentTarget.value as StatusTypeForSelect)
+	const onChangeActivityFilterHandler = (e: ChangeEvent<HTMLSelectElement>) => {
+
+		props.setActivityFilter(e.currentTarget.value as StatusTypeForSelect)
+
+	}
+
+	const changeStatusHandler = (wishId: string , e: ChangeEvent<HTMLInputElement>) => {
+			props.changeWishStatus( wishId,e.currentTarget.checked)
 	}
 
 	return (
@@ -58,7 +69,10 @@ export const WishList = (props: WishListPropsType) => {
 						   value={props.newWishTitle}
 						   onChange={onNewItemChangeHandler}
 						   onKeyDown={onKeyDownHandler}
+
 					/>
+					{ error === "Select item" ?  <div>{error}</div> : "" }
+
 				</div>
 				<div>
 					<select value={oS} onChange={onChangeOSHandler}>
@@ -66,6 +80,7 @@ export const WishList = (props: WishListPropsType) => {
 						<option value={"Android"}>Android</option>
 						<option value={"iOS"}>iOS</option>
 					</select>
+					{ error === "Select OS" ?  <div>{error}</div> : "" }
 				</div>
 				<div>
 					<button onClick={addWishHandler}>Add</button>
@@ -75,7 +90,7 @@ export const WishList = (props: WishListPropsType) => {
 				{props.wishes.map(el => {
 					return (
 						<li key={el.id}>
-							<input type="checkbox" checked={el.checked}/>
+							<input type="checkbox" checked={el.checked} onChange={(event)=>changeStatusHandler(el.id, event)}/>
 							<span> {el.title} </span>
 							<span> / OS: </span>
 							<span> {el.OS} </span>
@@ -98,7 +113,7 @@ export const WishList = (props: WishListPropsType) => {
 				<div>
 					FILTER BY ACTIVITY:
 					<div>
-						<select value={props.statusFilter} onChange={onChangeFilterStatusHandler}>
+						<select value={props.activityFilter} onChange={onChangeActivityFilterHandler}>
 							<option value={"All"}>All</option>
 							<option value={"Active"}>Active</option>
 							<option value={"Completed"}>Completed</option>
