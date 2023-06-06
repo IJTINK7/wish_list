8634a8bbdf1c174be08d632f71a7f74ea8b93455
IJTINK7 <ijtink7@mail.ru>
2023-06-06 21:36:55 +0300

    Formatting the code

--- src/App.tsx
+++ src/App.tsx
@@ -2,25 +2,18 @@ import React, {useState} from 'react';
 import './App.css';
 import {OsTypeForSelect, StatusTypeForSelect, WishList} from "./WishList";
 import {v1} from "uuid";
-
 export type OsType = "All" | "iOS" | "Android" | OsTypeForSelect
-
 export type WishlistType = {
-id: string, category: string, filterBy: string
+	id: string, category: string, filterBy: string
 }
-
-export type WishType = {id: string, title: string, OS?: string, genre?: string, checked: boolean}
-
+export type WishType = { id: string, title: string, OS?: string, genre?: string, checked: boolean }
 export type WishesDataType = {
 	[key: string]: WishType[]
 }
-
-
-
 function App() {
 
 	const wishlistID1 = v1();
-	const wishlistID2= v1()
+	const wishlistID2 = v1()
 
 	const [wishLists, setWishlists] = useState<WishlistType[]>([
 			{id: wishlistID1, category: "phones", filterBy: "OS"},
@@ -30,98 +23,54 @@ function App() {
 	const [wishes, setWishes] = useState<WishesDataType>(
 		{
 			[wishlistID1]: [
-				{id:v1(), title: 'Samsung Galaxy S23', OS: "Android", checked: true},
-				{id:v1(), title: 'IPhone 13 ProMax', OS: "iOS", checked: true},
-				{id:v1(), title: 'Xiaomi 13', OS: "Android", checked: true},
-				{id:v1(), title: 'Huawei', OS: "Android", checked: false},
-				{id:v1(), title: 'Iphone 14', OS: "iOS", checked: false}
+				{id: v1(), title: 'Samsung Galaxy S23', OS: "Android", checked: true},
+				{id: v1(), title: 'IPhone 13 ProMax', OS: "iOS", checked: true},
+				{id: v1(), title: 'Xiaomi 13', OS: "Android", checked: true},
+				{id: v1(), title: 'Huawei', OS: "Android", checked: false},
+				{id: v1(), title: 'Iphone 14', OS: "iOS", checked: false}
 
 			],
-			[wishlistID2 ]: [
+			[wishlistID2]: [
 
-				{id:v1(), title: 'Hamlet ', genre: "Drama", checked: true},
-				{id:v1(), title: 'The Odyssey ', genre: "History", checked: true},
-				{id:v1(), title: 'Sherlock Holmes', genre: "Detective", checked: true},
-				{id:v1(), title: 'Don Quixote', genre: "Adventure", checked: false},
-				{id:v1(), title: 'HeadFirst JS', genre: "Sceince", checked: false}]
-
-
-		} )
+				{id: v1(), title: 'Hamlet ', genre: "Drama", checked: true},
+				{id: v1(), title: 'The Odyssey ', genre: "History", checked: true},
+				{id: v1(), title: 'Sherlock Holmes', genre: "Detective", checked: true},
+				{id: v1(), title: 'Don Quixote', genre: "Adventure", checked: false},
+				{id: v1(), title: 'HeadFirst JS', genre: "Sceince", checked: false}]
 
 
+		})
 	const [activityFilter, setActivityFilter] = useState<StatusTypeForSelect>("All")
 	const [osFilter, setOsFilter] = useState<OsType>("All")
-	// const [wishes, setWishes] = useState<WishesDataPropsType[]>([
-	// 	{id: v1(), title: 'Samsung Galaxy S23', OS: "Android", checked: true},
-	// 	{id: v1(), title: 'IPhone 13 ProMax', OS: "iOS", checked: true},
-	// 	{id: v1(), title: 'Xiaomi 13', OS: "Android", checked: true},
-	// 	{id: v1(), title: 'Huawei', OS: "Android", checked: false},
-	// 	{id: v1(), title: 'Iphone 14', OS: "iOS", checked: false},
-	// ])
-	//
+
 	const addNewWish = (wishlistId: string, oS: OsTypeForSelect, newValue: string) => {
-
-		let newItem = {id:v1(), title: newValue, OS: "iOS", checked: false}
-		let newItem2 = {id:v1(), title: newValue, genre: "Drama", checked: false}
-
-
-		wishLists.forEach(el=> {
-
+		let newItem = {id: v1(), title: newValue, OS: "iOS", checked: false}
+		let newItem2 = {id: v1(), title: newValue, genre: "Drama", checked: false}
+		wishLists.forEach(el => {
 			if (el.filterBy === "OS") {
 				setWishes({...wishes, [wishlistId]: [newItem, ...wishes[wishlistId]]})
-			}
-			else {
+			} else {
 				setWishes({...wishes, [wishlistId]: [newItem2, ...wishes[wishlistId]]})
-
 			}
 		})
-
-
-
 	}
-
-
-
-
-
-
-
-
 	const removeWish = (wishlistID: string, id: string) => {
 
-		setWishes({...wishes, [wishlistID]: wishes[wishlistID].filter(el=> el.id !== id)})
+		setWishes({...wishes, [wishlistID]: wishes[wishlistID].filter(el => el.id !== id)})
 
 
 	}
+	const changeWishStatus = (wishlistID: string, wishId: string, statusValue: boolean) => {
+		setWishes({
+			...wishes,
+			[wishlistID]: wishes[wishlistID].map(el => el.id === wishId ? {...el, checked: statusValue} : el)
+		})
 
-	// //{id: v1(), title: 'Samsung Galaxy S23', OS: "Android", checked: true, isTrue: statusValue }
-	// //1. Видишь объект-делай копию. 2. Видишь массив-делай копию. 3. Видишь ключ-создавай новый.
-	//
-	const changeWishStatus = (wishlistID: string,wishId: string, statusValue: boolean) => {
-		setWishes({...wishes, [wishlistID]: wishes[wishlistID].map(el=>el.id === wishId ?  {...el, checked : statusValue}   : el    )})
-		// setWishes(wishes.map((el)=> el.id === wishId ? {...el, checked: statusValue} :el))
 	}
-	//
-	//
-	//
-	//
-	// const wishesWhatWeWantToSee = (osFilter === "All" ? wishes : wishes.filter(el => el.OS === osFilter)) // select OS
-	//
-	//
-	// const wishesWhatWeWantToSeeGeneral = activityFilter === "All" ?
-	// 	wishesWhatWeWantToSee : activityFilter === "Active" ?
-	// 	wishesWhatWeWantToSee.filter(el=> !el.checked )
-	// 	: wishesWhatWeWantToSee.filter(el=> el.checked )
-	// // select Activity
-	//
-
-
 	return (
-
-
 		<div className="App">
-			{wishLists.map((wl)=> {
-				return 	<WishList
+			{wishLists.map((wl) => {
+				return <WishList
 					key={wl.id}
 					wishlistID={wl.id}
 					wishes={wishes}
@@ -135,12 +84,8 @@ function App() {
 					category={wl.category}
 
 				/>
-			}  )}
-
+			})}
 		</div>
-
-
-
 	);
 }
 
--- src/WishList.tsx
+++ src/WishList.tsx
@@ -3,10 +3,8 @@ import {OsType, WishesDataType, WishType} from "./App";
 import {SuperInput} from "./superComponents/SuperInput";
 import SuperCheckbox from "./superComponents/SuperCheckbox";
 import {SuperSelect} from "./superComponents/SuperSelect";
-
 export type OsTypeForSelect = "Android" | "iOS" | "Select OS"
 export type StatusTypeForSelect = "All" | "Active" | "Completed"
-
 export type WishListPropsType = {
 	wishes:WishesDataType
 	osFilter: OsType
@@ -19,15 +17,9 @@ export type WishListPropsType = {
 	wishlistID: string
 	category: string
 }
-
 export const WishList = (props: WishListPropsType) => {
 	const [error, setError] = useState<string | null>(null)
 	const [oS, setOS] = useState<OsTypeForSelect>("Select OS")
-	// const onNewItemChangeHandler = (e: ChangeEvent<HTMLInputElement>) => {
-	// 	props.setNewWishTitle(e.currentTarget.value)
-	// 	setError(null)
-	// }
-
 	const addWishHandler = (newValue: string) => {
 		if (oS !== "Select OS") {
 			if (newValue.trim() !== ""){
@@ -38,17 +30,9 @@ export const WishList = (props: WishListPropsType) => {
 			} else setError("Select item")
 		} else setError("Select OS")
 	}
-
-
-
-
-
 	const removeWishHandler = (id: string) => {
 		props.removeWish(props.wishlistID ,id)
 	}
-
-
-
 	const onChangeOSHandler = (value: string) => {
 		setOS(value as OsTypeForSelect)
 		setError(null)
@@ -61,45 +45,27 @@ export const WishList = (props: WishListPropsType) => {
 		props.setActivityFilter(value as StatusTypeForSelect)
 
 	}
-
 	const changeStatusHandler = (wishId: string , value: boolean) => {
 			props.changeWishStatus( props.wishlistID, wishId, value)
 	}
-
 	return (
 		<div>
 			<h1>{props.category}</h1>
 			<div style={{display: "flex", justifyContent: "space-between"}}>
 				<div>
 					<SuperInput callBack={addWishHandler}  setError={setError}/>
-					{/*<input placeholder={"Enter an item"}*/}
-					{/*	   value={props.newWishTitle}*/}
-					{/*	   onChange={onNewItemChangeHandler}*/}
-
-
-					{/*/>*/}
 					{ error === "Select item" ?  <div>{error}</div> : "" }
 
 				</div>
 				<div>
-					{/*<select value={oS} onChange={onChangeOSHandler}>*/}
-					{/*	<option value={"Select OS"}>Select OS</option>*/}
-					{/*	<option value={"Android"}>Android</option>*/}
-					{/*	<option value={"iOS"}>iOS</option>*/}
-					{/*</select>*/}
 					<SuperSelect options = {[{value: 'Select OS', label: "Select OS"}, {value: 'Android', label: "Android"}, {value: 'iOS', label: "iOS"}]}  callBack={onChangeOSHandler} />
 					{ error === "Select OS" ?  <div>{error}</div> : "" }
-				</div>
-				<div>
-					{/*<button onClick={addWishHandler}>Add</button>*/}
 				</div>
 			</div>
 			<ul>
 				{props.wishes[props.wishlistID].map((el: WishType, index: number) => {
 					return (
 						<li key={index}>
-
-	{/*<input type="checkbox" checked={el.checked} onChange={(event)=>changeStatusHandler(el.id, event)}/>*/}
 	<SuperCheckbox checked={el.checked} callBack={(value)=>{changeStatusHandler(el.id, value)}}		 />
 							<span> {el.title} </span>
 							<span> / OS: </span>
@@ -112,23 +78,13 @@ export const WishList = (props: WishListPropsType) => {
 			<div style={{display: "flex", justifyContent: "space-between"}}>
 				<div style={{marginRight: "20px"}}>
 					FILTER BY OS:
-					{/*<div>*/}
-					{/*	<select value={props.osFilter} onChange={onChangeFilterOSHandler}>*/}
-					{/*		<option value={"All"}>All</option>*/}
-					{/*		<option value={"Android"}>Android</option>*/}
-					{/*		<option value={"iOS"}>iOS</option>*/}
-					{/*	</select>*/}
-					{/*</div>*/}
 					<SuperSelect options = {[{value: 'All', label: "All"}, {value: 'Android', label: "Android"}, {value: 'iOS', label: "iOS"}]} callBack={onChangeFilterOSHandler} />
 				</div>
 				<div>
 					FILTER BY ACTIVITY:
-
 					<div>
 						<SuperSelect options = {[{value: 'All', label: "All"}, {value: 'Active', label: "Active"}, {value: 'Completed', label: "Completed"}]} callBack={onChangeActivityFilterHandler}/>
 					</div>
-
-
 				</div>
 			</div>
 		</div>
